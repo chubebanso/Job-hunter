5ad3ae9d27fb496aad0d82c08f0e9fc09d840621
AnhLoiChu <chuanhloilee@gmail.com>
2024-12-04 02:18:48 +0700

    UserProfile

---
+++ Frontend/frontend/src/components/AppliedJobTable.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './ui/table'
+import { Badge } from './ui/badge'
+import { useSelector } from 'react-redux'
+
+const AppliedJobTable = () => {
+    return (
+        <div>
+            <Table>
+                <TableCaption>A list of your applied jobs</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Date</TableHead>
+                        <TableHead>Job Role</TableHead>
+                        <TableHead>Company</TableHead>
+                        <TableHead className="text-right">Status</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {
+                        [1,2,3,4].map((item,index) => (
+                            <TableRow key={index}>
+                                <TableCell>11-06-2000</TableCell>
+                                <TableCell>dev fe</TableCell>
+                                <TableCell>CocCoc</TableCell>
+                                <TableCell className="text-right"><Badge className>Selected</Badge></TableCell>
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
+            </Table>
+        </div>
+    )
+}
+
+export default AppliedJobTable
